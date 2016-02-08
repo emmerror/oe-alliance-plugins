5a931fd9fdcbaf728562e0fa07c3e6ad74b3208a
emmerror <emmerror@users.noreply.github.com>
2016-02-08 19:56:11 +0100

    Update plugin.py
    IPTV fix

--- ReZap/src/plugin.py
+++ ReZap/src/plugin.py
@@ -11,11 +11,27 @@ class LoopSyncMain(Screen):
 		self.gotSession()
 
 	def gotSession(self):
+		self.ttype = "DVB"
 		self.AVSyncTimer = eTimer()
 		self.AVSyncTimer.callback.append(self.UpdateStatus)
 		self.AVSyncTimer.start(10000, True)
 
 	def UpdateStatus(self):
+		frontendDataOrg = ""
+		service1 = self.session.nav.getCurrentlyPlayingServiceOrGroup()
+		if service1:
+			service = self.session.nav.getCurrentService()
+			if service:
+				feinfo = service.frontendInfo()
+				frontendDataOrg = feinfo and feinfo.getAll(True)
+				if not len(frontendDataOrg): 
+					self.ttype = "IPTV"
+					self.AVSyncTimer.start(500, True)
+					return
+				if self.ttype == "IPTV": 
+					self.ttype = "DVB"
+					self.AVSyncTimer.start(100, True)
+					return
 		rst_status = 0
 		try:
 			f = open("/sys/class/tsync/reset_flag", "r")
@@ -23,16 +39,22 @@ class LoopSyncMain(Screen):
 			f.close()
 		except Exception, e:
 			print "[ReZap] Can't read class"
-			self.AVSyncTimer.start(300, True)
+			self.AVSyncTimer.start(500, True)
 			return
 		if rst_status == 1 :
 			print "[ReZap] DoReZap !!!"
 			rst_status = 0
-			self.session.open(DoReZap)
-			self.AVSyncTimer.start(5000, True)
+			self.AVSyncTimer.start(500, True)
+			try:
+				f_tmp = open("/sys/class/tsync/reset_flag", "w")
+				f_tmp.write("0")
+				f_tmp.close()
+				self.session.open(DoReZap,service1)
+			except Exception, e:
+				print "[ReZap] Can't DoReZap"
 			return
 		self.AVSyncTimer.start(100, True)
-               
+
 ###################################                
 class DoReZap(Screen):
   
@@ -40,21 +62,17 @@ class DoReZap(Screen):
 		<screen position="center,center" size="1920,1080" title="" >
 		</screen>"""
 
-	def __init__(self, session):
+	def __init__(self, session, xxx):
 		Screen.__init__(self, session)
 
 		try:
 			f_tmp = open("/sys/class/video/blackout_policy", "w")
 			f_tmp.write("0")
 			f_tmp.close()
-			f_tmp = open("/sys/class/tsync/reset_flag", "w")
-			f_tmp.write("0")
-			f_tmp.close()
 		except Exception, e:
 			print "[ReZap] Can't change policy(0)"
-		self.current_service		= self.session.nav.getCurrentlyPlayingServiceReference()
 		self.session.nav.stopService()
-		self.session.nav.playService(self.current_service)
+		self.session.nav.playService(xxx)
 		try:
 			f_tmp = open("/sys/class/video/blackout_policy", "w")
 			f_tmp.write("1")
